65b18c74ae4f1be320908538351de3ae75b6190a
Yuki Kimoto <kimoto.yuki@gmail.com>
2014-05-30 10:41:43 +0900

    add files

---
+++ lib/Data/R.pm
@@ -0,0 +1,7 @@
+package Data::R;
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+Data::R - R-like statistical library
---
+++ t/basic.t
@@ -0,0 +1,5 @@
+use Test::More 'no_plan';
+use strict;
+use warnings;
+
+ok('test');

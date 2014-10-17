3d3db2c6f57041d019e3f80838a0c7abbd67ee1a
Yuki Kimoto <kimoto.yuki@gmail.com>
2014-10-17 14:53:47 +0900

    fix complex as_xxx bug

--- lib/Rstats/Element.pm
+++ lib/Rstats/Element.pm
@@ -119,7 +119,7 @@ sub as_double {
   }
   elsif ($self->is_complex) {
     carp "imaginary parts discarded in coercion";
-    return Rstats::ElementFunc::double($self->re->value);
+    return Rstats::ElementFunc::double($self->re);
   }
   elsif ($self->is_double) {
     return $self;
@@ -152,7 +152,7 @@ sub as_integer {
   }
   elsif ($self->is_complex) {
     carp "imaginary parts discarded in coercion";
-    return Rstats::ElementFunc::integer(int($self->re->value));
+    return Rstats::ElementFunc::integer(int($self->re));
   }
   elsif ($self->is_double) {
     if ($self->is_nan || $self->is_infinite) {
@@ -191,8 +191,8 @@ sub as_logical {
   }
   elsif ($self->is_complex) {
     carp "imaginary parts discarded in coercion";
-    my $re = $self->re->value;
-    my $im = $self->im->value;
+    my $re = $self->re;
+    my $im = $self->im;
     if (defined $re && $re == 0 && defined $im && $im == 0) {
       return Rstats::ElementFunc::FALSE();
     }
@@ -353,8 +353,8 @@ sub value {
   }
   elsif ($self->is_complex) {
     return {
-      re => $self->re->value,
-      im => $self->im->value
+      re => $self->re,
+      im => $self->im
     };
   }
   elsif ($self->is_character) {

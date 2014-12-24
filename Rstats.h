b68df1eed06540bee64ed8b34aaeefe08cdae430
Yuki Kimoto <kimoto.yuki@gmail.com>
2014-12-24 19:08:57 +0900

    cleanup log, logb

--- Rstats.h
+++ Rstats.h
@@ -269,6 +269,16 @@ namespace Rstats {
     NV Mod(std::complex<NV> e1) { return abs(e1); }
     NV Mod(NV e1) { return abs(e1); }
     NV Mod(IV e1) { return abs((NV)e1); }
+
+    // log
+    std::complex<NV> log(std::complex<NV> e1) { return std::log(e1); }
+    NV log(NV e1) { return std::log(e1); }
+    NV log(IV e1) { return log((NV)e1); }
+
+    // logb
+    std::complex<NV> logb(std::complex<NV> e1) { return log(e1); }
+    NV logb(NV e1) { return log(e1); }
+    NV logb(IV e1) { return log((NV)e1); }
   }
   
   // Macro for Rstats::Vector
@@ -3090,48 +3100,10 @@ namespace Rstats {
       
       return e2;
     }
-        
-    Rstats::Vector* log(Rstats::Vector* e1) {
-      
-      IV length = e1->get_length();
-      Rstats::Vector* e2;
-      Rstats::VectorType::Enum type = e1->get_type();
-      switch (type) {
-        case Rstats::VectorType::CHARACTER :
-          croak("Error in a - b : non-numeric argument to binary operator");
-          break;
-        case Rstats::VectorType::COMPLEX :
-          e2 = Rstats::Vector::new_complex(length);
-          for (IV i = 0; i < length; i++) {
-            e2->set_complex_value(i, std::log(e1->get_complex_value(i)));
-          }
-          break;
-        case Rstats::VectorType::DOUBLE :
-          e2 = Rstats::Vector::new_double(length);
-          for (IV i = 0; i < length; i++) {
-            e2->set_double_value(i, std::log(e1->get_double_value(i)));
-          }
-          break;
-        case Rstats::VectorType::INTEGER :
-        case Rstats::VectorType::LOGICAL :
-          e2 = Rstats::Vector::new_double(length);
-          for (IV i = 0; i < length; i++) {
-            e2->set_double_value(i, std::log(e1->get_integer_value(i)));
-          }
-          break;
-        default:
-          croak("Invalid type");
 
-      }
+    RSTATS_DEF_VECTOR_FUNC_UN_MATH_INTEGER_TO_DOUBLE(log, Rstats::ElementFunc::log)
 
-      e2->merge_na_positions(e1);
-      
-      return e2;
-    }
-
-    Rstats::Vector* logb(Rstats::Vector* e1) {
-      return log(e1);
-    }
+    RSTATS_DEF_VECTOR_FUNC_UN_MATH_INTEGER_TO_DOUBLE(logb, Rstats::ElementFunc::logb)
     
     Rstats::Vector* log10(Rstats::Vector* e1) {
       

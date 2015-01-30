973e7b2e67983b5103d08b3a4f632f01a4fd4e84
Yuki Kimoto <kimoto.yuki@gmail.com>
2015-01-30 11:59:27 +0900

    cleanup list

--- lib/Rstats/List.pm
+++ lib/Rstats/List.pm
@@ -1,134 +1,15 @@
 package Rstats::List;
 use Rstats::Object -base;
 
-use Rstats::Func;
-use Carp 'croak';
+use Rstats::ListFunc;
 
 use overload '""' => \&to_string,
   fallback => 1;
 
-sub getin {
-  my ($x1, $_index) = @_;
-  
-  unless (defined $_index) {
-    $_index = $x1->at;
-  }
-  $x1->at($_index);
-  
-  my $x1_index = Rstats::Func::to_c($x1, $_index);
-  my $index;
-  if (Rstats::Func::is_character($x1, $x1_index)) {
-    $index = $x1->_name_to_index($x1_index);
-  }
-  else {
-    $index = $x1_index->values->[0];
-  }
-  my $elements = $x1->list;
-  my $element = $elements->[$index - 1];
-  
-  return $element;
-}
-
-sub get {
-  my $x1 = shift;
-  my $index = Rstats::Func::to_c($x1, shift);
-  
-  my $elements = $x1->list;
-  
-  my $class = ref $x1;
-  my $list = $class->new;
-  my $list_elements = $list->list;
-  
-  my $index_values;
-  if (Rstats::Func::is_character($x1, $index)) {
-    $index_values = [];
-    for my $value (@{$index->values}) {
-      push @$index_values, $x1->_name_to_index($value);
-    }
-  }
-  else {
-    $index_values = $index->values;
-  }
-  for my $i (@{$index_values}) {
-    push @$list_elements, $elements->[$i - 1];
-  }
-  
-  $x1->copy_attrs_to($list, {new_indexes => [Rstats::ArrayFunc::c($x1, @$index_values)]});
-
-  return $list;
-}
-
-sub set {
-  my ($x1, $v1) = @_;
-  
-  my $_index = $x1->at;
-  my $x1_index = Rstats::Func::to_c($x1, @$_index);
-  my $index;
-  if (Rstats::Func::is_character($x1, $x1_index)) {
-    $index = $x1->_name_to_index($x1_index);
-  }
-  else {
-    $index = $x1_index->values->[0];
-  }
-  $v1 = Rstats::Func::to_c($x1, $v1);
-  
-  if (Rstats::Func::is_null($x1, $v1)) {
-    splice @{$x1->list}, $index - 1, 1;
-    if (exists $x1->{names}) {
-      my $new_names_values = $x1->{names}->values;
-      splice @$new_names_values, $index - 1, 1;
-      $x1->{names} = Rstats::VectorFunc::new_character(@$new_names_values);
-    }
-    
-    if (exists $x1->{dimnames}) {
-      my $new_dimname_values = $x1->{dimnames}[1]->values;
-      splice @$new_dimname_values, $index - 1, 1;
-      $x1->{dimnames}[1] = Rstats::VectorFunc::new_character(@$new_dimname_values);
-    }
-  }
-  else {
-    if ($x1->is_data_frame) {
-      my $x1_length = $x1->length_value;
-      my $v1_length = $v1->length_value;
-      if ($x1_length != $v1_length) {
-        croak "Error in data_frame set: replacement has $v1_length rows, data has $x1_length";
-      }
-    }
-    
-    $x1->list->[$index - 1] = $v1;
-  }
-  
-  return $x1;
-}
-
-sub to_string {
-  my $x1 = shift;
-  
-  my $poses = [];
-  my $str = '';
-  $x1->_to_string($x1, $poses, \$str);
-  
-  return $str;
-}
-
-sub _to_string {
-  my ($x1, $list, $poses, $str_ref) = @_;
-  
-  my $elements = $list->list;
-  for (my $i = 0; $i < @$elements; $i++) {
-    push @$poses, $i + 1;
-    $$str_ref .= join('', map { "[[$_]]" } @$poses) . "\n";
-    
-    my $element = $elements->[$i];
-    if (ref $element eq 'Rstats::List') {
-      $x1->_to_string($element, $poses, $str_ref);
-    }
-    else {
-      $$str_ref .= $element->to_string . "\n";
-    }
-    pop @$poses;
-  }
-}
+sub getin { Rstats::ListFunc::getin(undef(), @_) }
+sub get { Rstats::ListFunc::get(undef(), @_) }
+sub set { Rstats::ListFunc::set(undef(), @_) }
+sub to_string { Rstats::ListFunc::to_string(undef(), @_) }
 
 1;
 
--- lib/Rstats/ListFunc.pm
+++ lib/Rstats/ListFunc.pm
@@ -1,4 +1,4 @@
-package Rstats::List;
+package Rstats::ListFunc;
 use Rstats::Object -base;
 
 use Rstats::Func;
@@ -8,22 +8,22 @@ use overload '""' => \&to_string,
   fallback => 1;
 
 sub getin {
-  my ($r, $_index) = @_;
+  my ($r, $x1, $_index) = @_;
   
   unless (defined $_index) {
-    $_index = $r->at;
+    $_index = $x1->at;
   }
-  $r->at($_index);
+  $x1->at($_index);
   
-  my $x1_index = Rstats::Func::to_c($r, $_index);
+  my $x1_index = Rstats::Func::to_c($x1, $_index);
   my $index;
-  if (Rstats::Func::is_character($r, $x1_index)) {
-    $index = $r->_name_to_index($x1_index);
+  if (Rstats::Func::is_character($x1, $x1_index)) {
+    $index = $x1->_name_to_index($x1_index);
   }
   else {
     $index = $x1_index->values->[0];
   }
-  my $elements = $r->list;
+  my $elements = $x1->list;
   my $element = $elements->[$index - 1];
   
   return $element;
@@ -31,19 +31,20 @@ sub getin {
 
 sub get {
   my $r = shift;
-  my $index = Rstats::Func::to_c($r, shift);
+  my $x1 = shift;
+  my $index = Rstats::Func::to_c($x1, shift);
   
-  my $elements = $r->list;
+  my $elements = $x1->list;
   
-  my $class = ref $r;
+  my $class = ref $x1;
   my $list = $class->new;
   my $list_elements = $list->list;
   
   my $index_values;
-  if (Rstats::Func::is_character($r, $index)) {
+  if (Rstats::Func::is_character($x1, $index)) {
     $index_values = [];
     for my $value (@{$index->values}) {
-      push @$index_values, $r->_name_to_index($value);
+      push @$index_values, $x1->_name_to_index($value);
     }
   }
   else {
@@ -53,60 +54,62 @@ sub get {
     push @$list_elements, $elements->[$i - 1];
   }
   
-  $r->copy_attrs_to($list, {new_indexes => [Rstats::ArrayFunc::c($r, @$index_values)]});
+  $x1->copy_attrs_to($list, {new_indexes => [Rstats::ArrayFunc::c($x1, @$index_values)]});
 
   return $list;
 }
 
 sub set {
-  my ($r, $v1) = @_;
+  my $r = shift;
+  my ($x1, $v1) = @_;
   
-  my $_index = $r->at;
-  my $x1_index = Rstats::Func::to_c($r, @$_index);
+  my $_index = $x1->at;
+  my $x1_index = Rstats::Func::to_c($x1, @$_index);
   my $index;
-  if (Rstats::Func::is_character($r, $x1_index)) {
-    $index = $r->_name_to_index($x1_index);
+  if (Rstats::Func::is_character($x1, $x1_index)) {
+    $index = $x1->_name_to_index($x1_index);
   }
   else {
     $index = $x1_index->values->[0];
   }
-  $v1 = Rstats::Func::to_c($r, $v1);
+  $v1 = Rstats::Func::to_c($x1, $v1);
   
-  if (Rstats::Func::is_null($r, $v1)) {
-    splice @{$r->list}, $index - 1, 1;
-    if (exists $r->{names}) {
-      my $new_names_values = $r->{names}->values;
+  if (Rstats::Func::is_null($x1, $v1)) {
+    splice @{$x1->list}, $index - 1, 1;
+    if (exists $x1->{names}) {
+      my $new_names_values = $x1->{names}->values;
       splice @$new_names_values, $index - 1, 1;
-      $r->{names} = Rstats::VectorFunc::new_character(@$new_names_values);
+      $x1->{names} = Rstats::VectorFunc::new_character(@$new_names_values);
     }
     
-    if (exists $r->{dimnames}) {
-      my $new_dimname_values = $r->{dimnames}[1]->values;
+    if (exists $x1->{dimnames}) {
+      my $new_dimname_values = $x1->{dimnames}[1]->values;
       splice @$new_dimname_values, $index - 1, 1;
-      $r->{dimnames}[1] = Rstats::VectorFunc::new_character(@$new_dimname_values);
+      $x1->{dimnames}[1] = Rstats::VectorFunc::new_character(@$new_dimname_values);
     }
   }
   else {
-    if ($r->is_data_frame) {
-      my $r_length = $r->length_value;
+    if ($x1->is_data_frame) {
+      my $x1_length = $x1->length_value;
       my $v1_length = $v1->length_value;
-      if ($r_length != $v1_length) {
-        croak "Error in data_frame set: replacement has $v1_length rows, data has $r_length";
+      if ($x1_length != $v1_length) {
+        croak "Error in data_frame set: replacement has $v1_length rows, data has $x1_length";
       }
     }
     
-    $r->list->[$index - 1] = $v1;
+    $x1->list->[$index - 1] = $v1;
   }
   
-  return $r;
+  return $x1;
 }
 
 sub to_string {
   my $r = shift;
+  my $x1 = shift;
   
   my $poses = [];
   my $str = '';
-  $r->_to_string($r, $poses, \$str);
+  _to_string($r, $x1, $poses, \$str);
   
   return $str;
 }
@@ -121,10 +124,10 @@ sub _to_string {
     
     my $element = $elements->[$i];
     if (ref $element eq 'Rstats::List') {
-      $r->_to_string($element, $poses, $str_ref);
+      _to_string($r, $element, $poses, $str_ref);
     }
     else {
-      $$str_ref .= $element->to_string . "\n";
+      $$str_ref .= Rstats::ArrayFunc::to_string($r, $element) . "\n";
     }
     pop @$poses;
   }

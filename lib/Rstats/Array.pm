dff44eedc39aa3507f9ab4d61f865e60f09d06ad
Yuki Kimoto <kimoto.yuki@gmail.com>
2015-02-02 18:38:10 +0900

    move overload logic to object

--- lib/Rstats/Array.pm
+++ lib/Rstats/Array.pm
@@ -1,26 +1,6 @@
 package Rstats::Array;
 use Rstats::Object -base;
 
-use overload
-  '+' => sub { Rstats::ArrayFunc::add(undef(), Rstats::ArrayFunc::_fix_pos(undef(), @_)) },
-  '-' => sub { Rstats::ArrayFunc::subtract(undef(), Rstats::ArrayFunc::_fix_pos(undef(), @_)) },
-  '*' => sub { Rstats::ArrayFunc::multiply(undef(), Rstats::ArrayFunc::_fix_pos(undef(), @_)) },
-  '/' => sub { Rstats::ArrayFunc::divide(undef(), Rstats::ArrayFunc::_fix_pos(undef(), @_)) },
-  '%' => sub { Rstats::ArrayFunc::remainder(undef(), Rstats::ArrayFunc::_fix_pos(undef(), @_)) },
-  '**' => sub { Rstats::ArrayFunc::pow(undef(), Rstats::ArrayFunc::_fix_pos(undef(), @_)) },
-  '<' => sub { Rstats::ArrayFunc::less_than(undef(), Rstats::ArrayFunc::_fix_pos(undef(), @_)) },
-  '<=' => sub { Rstats::ArrayFunc::less_than_or_equal(undef(), Rstats::ArrayFunc::_fix_pos(undef(), @_)) },
-  '>' => sub { Rstats::ArrayFunc::more_than(undef(), Rstats::ArrayFunc::_fix_pos(undef(), @_)) },
-  '>=' => sub { Rstats::ArrayFunc::more_than_or_equal(undef(), Rstats::ArrayFunc::_fix_pos(undef(), @_)) },
-  '==' => sub { Rstats::ArrayFunc::equal(undef(), Rstats::ArrayFunc::_fix_pos(undef(), @_)) },
-  '!=' => sub { Rstats::ArrayFunc::not_equal(undef(), Rstats::ArrayFunc::_fix_pos(undef(), @_)) },
-  '&' => sub { Rstats::ArrayFunc::and(undef(), Rstats::ArrayFunc::_fix_pos(undef(), @_)) },
-  '|' => sub { Rstats::ArrayFunc::or(undef(), Rstats::ArrayFunc::_fix_pos(undef(), @_)) },
-  'x' => sub { Rstats::ArrayFunc::inner_product(undef(), Rstats::ArrayFunc::_fix_pos(undef(), @_)) },
-  bool => sub { Rstats::ArrayFunc::bool(undef(), @_) },
-  'neg' => sub { Rstats::ArrayFunc::negation(undef(), @_) },
-  fallback => 1;
-
 sub bool { Rstats::ArrayFunc::bool(undef(), @_) }
 sub value { Rstats::ArrayFunc::value(undef(), @_) }
 
--- lib/Rstats/Object.pm
+++ lib/Rstats/Object.pm
@@ -2,6 +2,23 @@ package Rstats::Object;
 use Object::Simple -base;
 
 use overload
+  '+' => sub { Rstats::ArrayFunc::add(undef(), Rstats::ArrayFunc::_fix_pos(undef(), @_)) },
+  '-' => sub { Rstats::ArrayFunc::subtract(undef(), Rstats::ArrayFunc::_fix_pos(undef(), @_)) },
+  '*' => sub { Rstats::ArrayFunc::multiply(undef(), Rstats::ArrayFunc::_fix_pos(undef(), @_)) },
+  '/' => sub { Rstats::ArrayFunc::divide(undef(), Rstats::ArrayFunc::_fix_pos(undef(), @_)) },
+  '%' => sub { Rstats::ArrayFunc::remainder(undef(), Rstats::ArrayFunc::_fix_pos(undef(), @_)) },
+  '**' => sub { Rstats::ArrayFunc::pow(undef(), Rstats::ArrayFunc::_fix_pos(undef(), @_)) },
+  '<' => sub { Rstats::ArrayFunc::less_than(undef(), Rstats::ArrayFunc::_fix_pos(undef(), @_)) },
+  '<=' => sub { Rstats::ArrayFunc::less_than_or_equal(undef(), Rstats::ArrayFunc::_fix_pos(undef(), @_)) },
+  '>' => sub { Rstats::ArrayFunc::more_than(undef(), Rstats::ArrayFunc::_fix_pos(undef(), @_)) },
+  '>=' => sub { Rstats::ArrayFunc::more_than_or_equal(undef(), Rstats::ArrayFunc::_fix_pos(undef(), @_)) },
+  '==' => sub { Rstats::ArrayFunc::equal(undef(), Rstats::ArrayFunc::_fix_pos(undef(), @_)) },
+  '!=' => sub { Rstats::ArrayFunc::not_equal(undef(), Rstats::ArrayFunc::_fix_pos(undef(), @_)) },
+  '&' => sub { Rstats::ArrayFunc::and(undef(), Rstats::ArrayFunc::_fix_pos(undef(), @_)) },
+  '|' => sub { Rstats::ArrayFunc::or(undef(), Rstats::ArrayFunc::_fix_pos(undef(), @_)) },
+  'x' => sub { Rstats::ArrayFunc::inner_product(undef(), Rstats::ArrayFunc::_fix_pos(undef(), @_)) },
+  bool => sub { Rstats::ArrayFunc::bool(undef(), @_) },
+  'neg' => sub { Rstats::ArrayFunc::negation(undef(), @_) },
   '""' => sub { shift->to_string },
   fallback => 1;
 

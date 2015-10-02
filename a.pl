55a1bfb6ac1cdaa4183aca10f8b500492c8484b7
Yuki Kimoto <kimoto.yuki@gmail.com>
2015-10-03 08:49:11 +0900

    remove VectorFunc::new_vector

---
+++ a.pl
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+use Rstats;
+
+my $inf = r->new_double('Inf');
+
+print $inf;
+
+1;
--- lib/Rstats/VectorFunc.pm
+++ lib/Rstats/VectorFunc.pm
@@ -2,33 +2,6 @@ package Rstats::VectorFunc;
 
 use strict;
 use warnings;
-use Carp 'croak', 'carp';
-
-require Rstats;
-use Rstats::Vector;
-
-sub new_vector {
-  my $type = shift;
-  
-  if ($type eq 'character') {
-    return new_character(@_);
-  }
-  elsif ($type eq 'complex') {
-    return new_complex(@_);
-  }
-  elsif ($type eq 'double') {
-    return new_double(@_);
-  }
-  elsif ($type eq 'integer') {
-    return new_integer(@_);
-  }
-  elsif ($type eq 'logical') {
-    return new_logical(@_);
-  }
-  else {
-    croak("Invalid type $type is passed(new_vector)");
-  }
-}
 
 1;
 =head1 NAME

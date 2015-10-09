ad99461893a944b415026274f7e7e7b5fbe58a56
Yuki Kimoto <kimoto.yuki@gmail.com>
2015-10-09 10:16:55 +0900

    cleanup complex

--- Rstats_ElementFunc.cpp
+++ Rstats_ElementFunc.cpp
@@ -5,32 +5,32 @@ namespace Rstats {
   namespace ElementFunc {
 
     // add
-    std::complex<NV> add(std::complex<NV> e1, std::complex<NV> e2) { return e1 + e2; }
+    Rstats::Complex add(Rstats::Complex e1, Rstats::Complex e2) { return e1 + e2; }
     NV add(NV e1, NV e2) { return e1 + e2; }
     IV add(IV e1, IV e2) { return e1 + e2; }
 
     // subtract
-    std::complex<NV> subtract(std::complex<NV> e1, std::complex<NV> e2) { return e1 - e2; }
+    Rstats::Complex subtract(Rstats::Complex e1, Rstats::Complex e2) { return e1 - e2; }
     NV subtract(NV e1, NV e2) { return e1 - e2; }
     IV subtract(IV e1, IV e2) { return e1 - e2; }
 
     // multiply
-    std::complex<NV> multiply(std::complex<NV> e1, std::complex<NV> e2) { return e1 * e2; }
+    Rstats::Complex multiply(Rstats::Complex e1, Rstats::Complex e2) { return e1 * e2; }
     NV multiply(NV e1, NV e2) { return e1 * e2; }
     IV multiply(IV e1, IV e2) { return e1 * e2; }
 
     // divide
-    std::complex<NV> divide(std::complex<NV> e1, std::complex<NV> e2) { return e1 / e2; }
+    Rstats::Complex divide(Rstats::Complex e1, Rstats::Complex e2) { return e1 / e2; }
     NV divide(NV e1, NV e2) { return e1 / e2; }
     NV divide(IV e1, IV e2) { return e1 / e2; }
 
     // pow
-    std::complex<NV> pow(std::complex<NV> e1, std::complex<NV> e2) { return std::pow(e1, e2); }
+    Rstats::Complex pow(Rstats::Complex e1, Rstats::Complex e2) { return std::pow(e1, e2); }
     NV pow(NV e1, NV e2) { return ::pow(e1, e2); }
     NV pow(IV e1, IV e2) { return pow((NV)e1, (NV)e2); }
 
     // remainder
-    std::complex<NV> remainder(std::complex<NV> e1, std::complex<NV> e2) {
+    Rstats::Complex remainder(Rstats::Complex e1, Rstats::Complex e2) {
       croak("unimplemented complex operation(Rstats::VectorFunc::remainder())");
     }
     NV remainder(NV e1, NV e2) {
@@ -49,53 +49,53 @@ namespace Rstats {
     }
 
     // Re
-    NV Re(std::complex<NV> e1) { return e1.real(); }
+    NV Re(Rstats::Complex e1) { return e1.real(); }
     NV Re(NV e1) { return e1; }
     NV Re(IV e1) { return e1; }
 
     // Im
-    NV Im(std::complex<NV> e1) { return e1.imag(); }
+    NV Im(Rstats::Complex e1) { return e1.imag(); }
     NV Im(NV e1) { return 0; }
     NV Im(IV e1) { return 0; }
 
     // Conj
-    std::complex<NV> Conj(std::complex<NV> e1) { return std::complex<NV>(e1.real(), -e1.imag()); }
+    Rstats::Complex Conj(Rstats::Complex e1) { return Rstats::Complex(e1.real(), -e1.imag()); }
     NV Conj(NV e1) { return e1; }
     NV Conj(IV e1) { return e1; }
 
     // sin
-    std::complex<NV> sin(std::complex<NV> e1) { return std::sin(e1); }
+    Rstats::Complex sin(Rstats::Complex e1) { return std::sin(e1); }
     NV sin(NV e1) { return std::sin(e1); }
     NV sin(IV e1) { return sin((NV)e1); }
 
     // cos
-    std::complex<NV> cos(std::complex<NV> e1) { return std::cos(e1); }
+    Rstats::Complex cos(Rstats::Complex e1) { return std::cos(e1); }
     NV cos(NV e1) { return std::cos(e1); }
     NV cos(IV e1) { return cos((NV)e1); }
 
     // tan
-    std::complex<NV> tan(std::complex<NV> e1) { return std::tan(e1); }
+    Rstats::Complex tan(Rstats::Complex e1) { return std::tan(e1); }
     NV tan(NV e1) { return std::tan(e1); }
     NV tan(IV e1) { return tan((NV)e1); }
 
     // sinh
-    std::complex<NV> sinh(std::complex<NV> e1) { return std::sinh(e1); }
+    Rstats::Complex sinh(Rstats::Complex e1) { return std::sinh(e1); }
     NV sinh(NV e1) { return std::sinh(e1); }
     NV sinh(IV e1) { return sinh((NV)e1); }
 
     // cosh
-    std::complex<NV> cosh(std::complex<NV> e1) { return std::cosh(e1); }
+    Rstats::Complex cosh(Rstats::Complex e1) { return std::cosh(e1); }
     NV cosh(NV e1) { return std::cosh(e1); }
     NV cosh(IV e1) { return cosh((NV)e1); }
 
     // tanh
-    std::complex<NV> tanh (std::complex<NV> z) {
+    Rstats::Complex tanh (Rstats::Complex z) {
       NV re = z.real();
       
       // For fix FreeBSD bug
       // FreeBAD return (NaN + NaNi) when real value is negative infinite
       if (std::isinf(re) && re < 0) {
-        return std::complex<NV>(-1, 0);
+        return Rstats::Complex(-1, 0);
       }
       else {
         return std::tanh(z);
@@ -105,45 +105,45 @@ namespace Rstats {
     NV tanh(IV e1) { return tanh((NV)e1); }
 
     // abs
-    NV abs(std::complex<NV> e1) { return std::abs(e1); }
+    NV abs(Rstats::Complex e1) { return std::abs(e1); }
     NV abs(NV e1) { return std::abs(e1); }
     NV abs(IV e1) { return abs((NV)e1); }
 
     // abs
-    NV Mod(std::complex<NV> e1) { return abs(e1); }
+    NV Mod(Rstats::Complex e1) { return abs(e1); }
     NV Mod(NV e1) { return abs(e1); }
     NV Mod(IV e1) { return abs((NV)e1); }
 
     // log
-    std::complex<NV> log(std::complex<NV> e1) { return std::log(e1); }
+    Rstats::Complex log(Rstats::Complex e1) { return std::log(e1); }
     NV log(NV e1) { return std::log(e1); }
     NV log(IV e1) { return log((NV)e1); }
 
     // logb
-    std::complex<NV> logb(std::complex<NV> e1) { return log(e1); }
+    Rstats::Complex logb(Rstats::Complex e1) { return log(e1); }
     NV logb(NV e1) { return log(e1); }
     NV logb(IV e1) { return log((NV)e1); }
 
     // log10
-    std::complex<NV> log10(std::complex<NV> e1) { return std::log10(e1); }
+    Rstats::Complex log10(Rstats::Complex e1) { return std::log10(e1); }
     NV log10(NV e1) { return std::log10(e1); }
     NV log10(IV e1) { return std::log10((NV)e1); }
 
     // log2
-    std::complex<NV> log2(std::complex<NV> e1) {
-      return std::log(e1) / std::log(std::complex<NV>(2, 0));
+    Rstats::Complex log2(Rstats::Complex e1) {
+      return std::log(e1) / std::log(Rstats::Complex(2, 0));
     }
     NV log2(NV e1) {
       return std::log(e1) / std::log((NV)2);
     }
     NV log2(IV e1) { return log2((NV)e1); }
 
-    std::complex<NV> expm1(std::complex<NV> e1) { croak("Error in expm1 : unimplemented complex function"); }
+    Rstats::Complex expm1(Rstats::Complex e1) { croak("Error in expm1 : unimplemented complex function"); }
     NV expm1(NV e1) { return ::expm1(e1); }
     NV expm1(IV e1) { return expm1((NV)e1); }
 
     // Arg
-    NV Arg(std::complex<NV> e1) {
+    NV Arg(Rstats::Complex e1) {
       NV re = e1.real();
       NV im = e1.imag();
       
@@ -158,15 +158,15 @@ namespace Rstats {
     NV Arg(IV e1) { return Arg((NV)e1); }
 
     // exp
-    std::complex<NV> exp(std::complex<NV> e1) { return std::exp(e1); }
+    Rstats::Complex exp(Rstats::Complex e1) { return std::exp(e1); }
     NV exp(NV e1) { return std::exp(e1); }
     NV exp(IV e1) { return exp((NV)e1); }
 
     // sqrt
-    std::complex<NV> sqrt(std::complex<NV> e1) {
+    Rstats::Complex sqrt(Rstats::Complex e1) {
       // Fix bug that clang sqrt can't right value of perfect squeres
       if (e1.imag() == 0 && e1.real() < 0) {
-        return std::complex<NV>(0, std::sqrt(-e1.real()));
+        return Rstats::Complex(0, std::sqrt(-e1.real()));
       }
       else {
         return std::sqrt(e1);
@@ -176,32 +176,32 @@ namespace Rstats {
     NV sqrt(IV e1) { return sqrt((NV)e1); }
 
     // atan
-    std::complex<NV> atan(std::complex<NV> e1) {
-      if (e1 == std::complex<NV>(0, 0)) {
-        return std::complex<NV>(0, 0);
+    Rstats::Complex atan(Rstats::Complex e1) {
+      if (e1 == Rstats::Complex(0, 0)) {
+        return Rstats::Complex(0, 0);
       }
-      else if (e1 == std::complex<NV>(0, 1)) {
-        return std::complex<NV>(0, INFINITY);
+      else if (e1 == Rstats::Complex(0, 1)) {
+        return Rstats::Complex(0, INFINITY);
       }
-      else if (e1 == std::complex<NV>(0, -1)) {
-        return std::complex<NV>(0, -INFINITY);
+      else if (e1 == Rstats::Complex(0, -1)) {
+        return Rstats::Complex(0, -INFINITY);
       }
       else {  
-        std::complex<NV> e2_i = std::complex<NV>(0, 1);
-        std::complex<NV> e2_log = std::log((e2_i + e1) / (e2_i - e1));
-        return (e2_i / std::complex<NV>(2, 0)) * e2_log;
+        Rstats::Complex e2_i = Rstats::Complex(0, 1);
+        Rstats::Complex e2_log = std::log((e2_i + e1) / (e2_i - e1));
+        return (e2_i / Rstats::Complex(2, 0)) * e2_log;
       }
     }
     NV atan(NV e1) { return ::atan2(e1, 1); }
     NV atan(IV e1) { return atan2((NV)e1, (NV)1); }
 
     // asin
-    std::complex<NV> asin(std::complex<NV> e1) {
+    Rstats::Complex asin(Rstats::Complex e1) {
       NV e1_re = e1.real();
       NV e1_im = e1.imag();
       
       if (e1_re == 0 && e1_im == 0) {
-        return std::complex<NV>(0, 0);
+        return Rstats::Complex(0, 0);
       }
       else {
         NV e2_t1 = std::sqrt(
@@ -244,19 +244,19 @@ namespace Rstats {
           e2_v = -e2_v;
         }
         
-        return std::complex<NV>(e2_u, e2_v);
+        return Rstats::Complex(e2_u, e2_v);
       }
     }
     NV asin(NV e1) { return std::asin(e1); }
     NV asin(IV e1) { return asin((NV)e1); }
 
     // acos
-    std::complex<NV> acos(std::complex<NV> e1) {
+    Rstats::Complex acos(Rstats::Complex e1) {
       NV e1_re = e1.real();
       NV e1_im = e1.imag();
       
       if (e1_re == 1 && e1_im == 0) {
-        return std::complex<NV>(0, 0);
+        return Rstats::Complex(0, 0);
       }
       else {
         NV e2_t1 = std::sqrt(
@@ -299,16 +299,16 @@ namespace Rstats {
           e2_v = -e2_v;
         }
         
-        return std::complex<NV>(e2_u, e2_v);
+        return Rstats::Complex(e2_u, e2_v);
       }
     }
     NV acos(NV e1) { return std::acos(e1); }
     NV acos(IV e1) { return acos((NV)e1); }
 
     // asinh
-    std::complex<NV> asinh(std::complex<NV> e1) {
-      std::complex<NV> e2_t = (
-        std::sqrt((e1 * e1) + std::complex<NV>(1, 0))
+    Rstats::Complex asinh(Rstats::Complex e1) {
+      Rstats::Complex e2_t = (
+        std::sqrt((e1 * e1) + Rstats::Complex(1, 0))
         +
         e1
       );
@@ -327,29 +327,29 @@ namespace Rstats {
     NV asinh(IV e1) { return asinh((NV)e1); }
 
     // acosh
-    std::complex<NV> acosh(std::complex<NV> e1) {
+    Rstats::Complex acosh(Rstats::Complex e1) {
       NV e1_re = e1.real();
       NV e1_im = e1.imag();
 
-      std::complex<NV> e2_t = (
+      Rstats::Complex e2_t = (
         std::sqrt(
           (e1 * e1)
           -
-          std::complex<NV>(1, 0)
+          Rstats::Complex(1, 0)
         )
         +
         e1
       );
-      std::complex<NV> e2_u = std::log(e2_t);
+      Rstats::Complex e2_u = std::log(e2_t);
       NV e2_re = e2_u.real();
       NV e2_im = e2_u.imag();
       
-      std::complex<NV> e2;
+      Rstats::Complex e2;
       if (e1_re < 0 && e1_im == 0) {
-        e2 = std::complex<NV>(e2_re, -e2_im);
+        e2 = Rstats::Complex(e2_re, -e2_im);
       }
       else {
-        e2 = std::complex<NV>(e2_re, e2_im);
+        e2 = Rstats::Complex(e2_re, e2_im);
       }
       
       if (e1_re < 0) {
@@ -381,22 +381,22 @@ namespace Rstats {
     NV acosh(IV e1) { return acosh((NV)e1); }
 
     // atanh
-    std::complex<NV> atanh(std::complex<NV> e1) {
-      if (e1 == std::complex<NV>(1, 0)) {
+    Rstats::Complex atanh(Rstats::Complex e1) {
+      if (e1 == Rstats::Complex(1, 0)) {
         warn("In atanh() : NaNs produced");
-        return std::complex<NV>(INFINITY, std::numeric_limits<NV>::signaling_NaN());
+        return Rstats::Complex(INFINITY, std::numeric_limits<NV>::signaling_NaN());
       }
-      else if (e1 == std::complex<NV>(-1, 0)) {
+      else if (e1 == Rstats::Complex(-1, 0)) {
         warn("In atanh() : NaNs produced");
-        return std::complex<NV>(-INFINITY, std::numeric_limits<NV>::signaling_NaN());
+        return Rstats::Complex(-INFINITY, std::numeric_limits<NV>::signaling_NaN());
       }
       else {
-        return std::complex<NV>(0.5, 0)
+        return Rstats::Complex(0.5, 0)
           *
           std::log(
-            (std::complex<NV>(1, 0) + e1)
+            (Rstats::Complex(1, 0) + e1)
             /
-            (std::complex<NV>(1, 0) - e1)
+            (Rstats::Complex(1, 0) - e1)
           );
       }
     }
@@ -424,19 +424,19 @@ namespace Rstats {
     NV atanh(IV e1) { return atanh((NV)e1); }
 
     // negation
-    std::complex<NV> negation(std::complex<NV> e1) { return -e1; }
+    Rstats::Complex negation(Rstats::Complex e1) { return -e1; }
     NV negation(NV e1) { return -e1; }
     IV negation(IV e1) { return -e1; }
 
     // atan2
-    std::complex<NV> atan2(std::complex<NV> e1, std::complex<NV> e2) {
-      std::complex<NV> e3_s = (e1 * e1) + (e2 * e2);
-      if (e3_s == std::complex<NV>(0, 0)) {
-        return std::complex<NV>(0, 0);
+    Rstats::Complex atan2(Rstats::Complex e1, Rstats::Complex e2) {
+      Rstats::Complex e3_s = (e1 * e1) + (e2 * e2);
+      if (e3_s == Rstats::Complex(0, 0)) {
+        return Rstats::Complex(0, 0);
       }
       else {
-        std::complex<NV> e3_i = std::complex<NV>(0, 1);
-        std::complex<NV> e3_r = e2 + (e1 * e3_i);
+        Rstats::Complex e3_i = Rstats::Complex(0, 1);
+        Rstats::Complex e3_r = e2 + (e1 * e3_i);
         return -e3_i * std::log(e3_r / std::sqrt(e3_s));
       }
     }
@@ -463,8 +463,8 @@ namespace Rstats {
 
     // And
     IV And(SV* e1, SV* e2) { croak("operations are possible only for numeric, logical or complex types"); }
-    IV And(std::complex<NV> e1, std::complex<NV> e2) {
-      if (e1 != std::complex<NV>(0, 0) && e2 != std::complex<NV>(0, 0)) { return 1; }
+    IV And(Rstats::Complex e1, Rstats::Complex e2) {
+      if (e1 != Rstats::Complex(0, 0) && e2 != Rstats::Complex(0, 0)) { return 1; }
       else { return 0; }
     }
     IV And(NV e1, NV e2) {
@@ -475,8 +475,8 @@ namespace Rstats {
 
     // Or
     IV Or(SV* e1, SV* e2) { croak("operations are possible only for numeric, logical or complex types"); }
-    IV Or(std::complex<NV> e1, std::complex<NV> e2) {
-      if (e1 != std::complex<NV>(0, 0) || e2 != std::complex<NV>(0, 0)) { return 1; }
+    IV Or(Rstats::Complex e1, Rstats::Complex e2) {
+      if (e1 != Rstats::Complex(0, 0) || e2 != Rstats::Complex(0, 0)) { return 1; }
       else { return 0; }
     }
     IV Or(NV e1, NV e2) {
@@ -487,7 +487,7 @@ namespace Rstats {
 
     // equal
     IV equal(SV* e1, SV* e2) { return sv_cmp(e1, e2) == 0 ? 1 : 0; }
-    IV equal(std::complex<NV> e1, std::complex<NV> e2) { return e1 == e2 ? 1 : 0; }
+    IV equal(Rstats::Complex e1, Rstats::Complex e2) { return e1 == e2 ? 1 : 0; }
     IV equal(NV e1, NV e2) {
       if (std::isnan(e1) || std::isnan(e2)) { throw "Can't compare NaN"; }
       else { return e1 == e2 ? 1 : 0; }
@@ -496,7 +496,7 @@ namespace Rstats {
 
     // not equal
     IV not_equal(SV* e1, SV* e2) { return sv_cmp(e1, e2) != 0 ? 1 : 0; }
-    IV not_equal(std::complex<NV> e1, std::complex<NV> e2) { return e1 != e2 ? 1 : 0; }
+    IV not_equal(Rstats::Complex e1, Rstats::Complex e2) { return e1 != e2 ? 1 : 0; }
     IV not_equal(NV e1, NV e2) {
       if (std::isnan(e1) || std::isnan(e2)) { throw "Can't compare NaN"; }
       else { return e1 != e2 ? 1 : 0; }
@@ -505,7 +505,7 @@ namespace Rstats {
 
     // more_than
     IV more_than(SV* e1, SV* e2) { return sv_cmp(e1, e2) > 0 ? 1 : 0; }
-    IV more_than(std::complex<NV> e1, std::complex<NV> e2) {
+    IV more_than(Rstats::Complex e1, Rstats::Complex e2) {
       croak("invalid comparison with complex values(more_than())");
     }
     IV more_than(NV e1, NV e2) {
@@ -516,7 +516,7 @@ namespace Rstats {
 
     // less_than
     IV less_than(SV* e1, SV* e2) { return sv_cmp(e1, e2) < 0 ? 1 : 0; }
-    IV less_than(std::complex<NV> e1, std::complex<NV> e2) {
+    IV less_than(Rstats::Complex e1, Rstats::Complex e2) {
       croak("invalid comparison with complex values(less_than())");
     }
     IV less_than(NV e1, NV e2) {
@@ -527,7 +527,7 @@ namespace Rstats {
 
     // more_than_or_equal
     IV more_than_or_equal(SV* e1, SV* e2) { return sv_cmp(e1, e2) >= 0 ? 1 : 0; }
-    IV more_than_or_equal(std::complex<NV> e1, std::complex<NV> e2) {
+    IV more_than_or_equal(Rstats::Complex e1, Rstats::Complex e2) {
       croak("invalid comparison with complex values(more_than_or_equal())");
     }
     IV more_than_or_equal(NV e1, NV e2) {
@@ -538,7 +538,7 @@ namespace Rstats {
 
     // less_than_or_equal
     IV less_than_or_equal(SV* e1, SV* e2) { return sv_cmp(e1, e2) <= 0 ? 1 : 0; }
-    IV less_than_or_equal(std::complex<NV> e1, std::complex<NV> e2) {
+    IV less_than_or_equal(Rstats::Complex e1, Rstats::Complex e2) {
       croak("invalid comparison with complex values(less_than_or_equal())");
     }
     IV less_than_or_equal(NV e1, NV e2) {
@@ -549,7 +549,7 @@ namespace Rstats {
 
     // is_infinite
     IV is_infinite(SV* e1) { return 0; }
-    IV is_infinite(std::complex<NV> e1) {
+    IV is_infinite(Rstats::Complex e1) {
       if (std::isnan(e1.real()) || std::isnan(e1.imag())) {
         return 0;
       }
@@ -565,7 +565,7 @@ namespace Rstats {
 
     // is_finite
     IV is_finite(SV* e1) { return 0; }
-    IV is_finite(std::complex<NV> e1) {
+    IV is_finite(Rstats::Complex e1) {
       if (std::isfinite(e1.real()) && std::isfinite(e1.imag())) {
         return 1;
       }
@@ -578,7 +578,7 @@ namespace Rstats {
 
     // is_nan
     IV is_nan(SV* e1) { return 0; }
-    IV is_nan(std::complex<NV> e1) {
+    IV is_nan(Rstats::Complex e1) {
       if (std::isnan(e1.real()) && std::isnan(e1.imag())) {
         return 1;
       }

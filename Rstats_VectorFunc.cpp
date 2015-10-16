47c03b57ad01f43e0e167e50b15b0d53396a4cf1
Yuki Kimoto <kimoto.yuki@gmail.com>
2015-10-16 16:33:47 +0900

    cleanup

--- Rstats_VectorFunc.cpp
+++ Rstats_VectorFunc.cpp
@@ -52,44 +52,6 @@ namespace Rstats {
       return v1;
     }
 
-    void delete_vector (Rstats::Vector* v1) {
-      
-      Rstats::Type::Enum type = Rstats::VectorFunc::get_type(v1);
-      
-      if (v1->values != NULL){ 
-        switch (type) {
-          case Rstats::Type::CHARACTER : {
-            std::vector<Rstats::Character>* values = v1->get_values<Rstats::Character>();
-            Rstats::Integer length = v1->get_values<Rstats::Character>()->size();
-            for (Rstats::Integer i = 0; i < length; i++) {
-              if ((*values)[i] != NULL) {
-                SvREFCNT_dec((*values)[i]);
-              }
-            }
-            delete values;
-            break;
-          }
-          case Rstats::Type::COMPLEX : {
-            std::vector<Rstats::Complex >* values = v1->get_values<Rstats::Complex>();
-            delete values;
-            break;
-          }
-          case Rstats::Type::DOUBLE : {
-            std::vector<Rstats::Double>* values = v1->get_values<Rstats::Double>();
-            delete values;
-            break;
-          }
-          case Rstats::Type::INTEGER :
-          case Rstats::Type::LOGICAL : {
-            std::vector<Rstats::Integer>* values = v1->get_values<Rstats::Integer>();
-            delete values;
-          }
-        }
-      }
-      
-      delete v1->na_positions;
-    }
-
     template <>
     void set_value<Rstats::Character>(Rstats::Vector* v1, Rstats::Integer pos, Rstats::Character value) {
       if (value != NULL) {

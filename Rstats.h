ccd4afc6407d799642bd9214cd55ac38235d86d3
Yuki Kimoto <kimoto.yuki@gmail.com>
2015-10-26 09:09:46 +0900

    improve subtract

--- Rstats.h
+++ Rstats.h
@@ -574,6 +574,13 @@ namespace Rstats {
       Rstats::Vector* v_out = Rstats::VectorFunc::operate_binary_math(func, v1, v2);
       return v_out;
     }
+
+    template <class T_IN, class T_OUT>
+    Rstats::Vector* subtract(Rstats::Vector* v1, Rstats::Vector* v2) {
+      T_OUT (*func)(T_IN, T_IN) = &Rstats::ElementFunc::subtract;
+      Rstats::Vector* v_out = Rstats::VectorFunc::operate_binary_math(func, v1, v2);
+      return v_out;
+    }
     
     template <class T_IN, class T_OUT>
     Rstats::Vector* as_character(Rstats::Vector* v1) {

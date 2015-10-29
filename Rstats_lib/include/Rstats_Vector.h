eafbaa30ed5eec2f69d8602c2b084ec10c14f187
Yuki Kimoto <kimoto.yuki@gmail.com>
2015-10-29 17:19:08 +0900

    cleanup templates

--- Rstats_lib/include/Rstats_Vector.h
+++ Rstats_lib/include/Rstats_Vector.h
@@ -14,35 +14,7 @@ namespace Rstats {
     
     public:
 
-    template<class T>
-    static Rstats::Vector* new_vector(Rstats::Integer);
-    
-    template <class T>
-    static Rstats::Vector* new_vector(Rstats::Integer length, T value) {
-      Rstats::Vector* v1 = new_vector<T>(length);
-      for (Rstats::Integer i = 0; i < length; i++) {
-        v1->set_value<T>(i, value);
-      }
-      return v1;
-    };
-        
     Rstats::Integer get_length();
-    
-    template<class T>
-    T* get_values() {
-      return (T*)this->values;
-    }
-
-    template<class T>
-    void set_value(Rstats::Integer pos, T value) {
-      *(this->get_values<T>() + pos) = value;
-    } // Rstats::Character is specialized
-
-    template <class T>
-    T get_value(Rstats::Integer pos) {
-      return *(this->get_values<T>() + pos);
-    }
-    
     void init_na_positions();
     void add_na_position(Rstats::Integer);
     Rstats::Logical exists_na_position(Rstats::Integer position);
@@ -52,15 +24,26 @@ namespace Rstats {
     
     ~Vector();
 
+    template<class T> // Specialized
+    static Rstats::Vector* new_vector(Rstats::Integer);
     template <class T>
-    void delete_vector() {
-      T* values = this->get_values<T>();
-      delete[] values;
-      delete[] this->na_positions;
-    }
+    static Rstats::Vector* new_vector(Rstats::Integer length, T value);
+    
+    template<class T>
+    T* get_values();
+    template<class T> // Rstats::Character is specialized
+    void set_value(Rstats::Integer pos, T value); 
+    template <class T>
+    T get_value(Rstats::Integer pos);
+    
+    template <class T> // Rstats::Character is specialized
+    void delete_vector();
   };
   template <>
   void Vector::set_value<Rstats::Character>(Rstats::Integer pos, Rstats::Character value);
+  template <>
+  void Vector::delete_vector<Rstats::Character>();
+
   template<>
   Rstats::Vector* Vector::new_vector<Rstats::Double>(Rstats::Integer);
   template<>
@@ -72,8 +55,7 @@ namespace Rstats {
   template<>
   Rstats::Vector* Vector::new_vector<Rstats::Logical>(Rstats::Integer);
 
-  template <>
-  void Vector::delete_vector<Rstats::Character>();
 }
+#include "Rstats_Vector_impl.h"
 
 #endif
--- Rstats_lib/include/Rstats_Vector_impl.h
+++ Rstats_lib/include/Rstats_Vector_impl.h
@@ -1,8 +1,33 @@
-template <class T>
-static Rstats::Vector* new_vector(Rstats::Integer length, T value) {
-  Rstats::Vector* v1 = new_vector<T>(length);
-  for (Rstats::Integer i = 0; i < length; i++) {
-    v1->set_value<T>(i, value);
+namespace Rstats {
+
+  template <class T>
+  Rstats::Vector* Vector::new_vector(Rstats::Integer length, T value) {
+    Rstats::Vector* v1 = new_vector<T>(length);
+    for (Rstats::Integer i = 0; i < length; i++) {
+      v1->set_value<T>(i, value);
+    }
+    return v1;
+  };
+
+  template<class T>
+  void Vector::set_value(Rstats::Integer pos, T value) {
+    *(this->get_values<T>() + pos) = value;
   }
-  return v1;
-};
+
+  template<class T>
+  T* Vector::get_values() {
+    return (T*)this->values;
+  }
+  
+  template <class T>
+  T Vector::get_value(Rstats::Integer pos) {
+    return *(this->get_values<T>() + pos);
+  }
+
+  template <class T>
+  void Vector::delete_vector() {
+    T* values = this->get_values<T>();
+    delete[] values;
+    delete[] this->na_positions;
+  }
+}

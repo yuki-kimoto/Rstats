b80fcccba769ac774a4c864a28cea0e43c8e46ac
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-10-04 22:08:21 +0900

    rename Rstats::Data to Rstats::Object

--- lib/Rstats/V2.pm
+++ lib/Rstats/V2.pm
@@ -9,15 +9,15 @@ use Rstats::Util ();
 use Digest::MD5 'md5_hex';
 use Rstats::Object;
 
-use SPVM 'Rstats::Data';
+use SPVM 'Rstats::Object';
 use SPVM 'Math';
 
 sub c {
   my ($r, $values) = @_;
   
-  my $r_data = SPVM::new_object("Rstats::Data");
+  my $r_data = SPVM::new_object("Rstats::Object");
   
-  $r_data->set(type => SPVM::Rstats::Data::TYPE_DOUBLE());
+  $r_data->set(type => SPVM::Rstats::Object::TYPE_DOUBLE());
   
   my $sp_values = SPVM::new_double_array($values);
   
@@ -29,9 +29,9 @@ sub c {
 sub pi {
   my ($r, $values) = @_;
   
-  my $r_data = SPVM::new_object("Rstats::Data");
+  my $r_data = SPVM::new_object("Rstats::Object");
   
-  $r_data->set(type => SPVM::Rstats::Data::TYPE_DOUBLE());
+  $r_data->set(type => SPVM::Rstats::Object::TYPE_DOUBLE());
   
   my $sp_values = SPVM::new_double_array([SPVM::Math::PI()]);
   

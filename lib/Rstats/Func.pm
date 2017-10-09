c01b687ceb6073e28c6a59ef2b22ff1760fb0dc4
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-10-09 10:12:07 +0900

    cleanup

--- lib/Rstats/Func.pm
+++ lib/Rstats/Func.pm
@@ -1368,7 +1368,7 @@ sub median {
     my $x4 = $x3->get($middle);
     my $x5 = $x3->get($middle + 1);
     
-    return $r->divide(($x4 + $x5), 2);
+    return $r->divide($r->add($x4, $x5), $r->c(2));
   }
   else {
     my $middle = int($x3_length / 2) + 1;
